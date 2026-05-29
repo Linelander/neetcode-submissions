d1671b00306de6b05dfdfa9967524f99987509dd
Connor Burch <118960765+Linelander@users.noreply.github.com>
2026-05-28 23:11:58 -0400

    Add: sum-of-two-integers - submission-16

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-16.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int getSum(int a, int b) {        
+        int r = 0;
+        int carry = 0;
+        for (int i = 0; i < 32; i++) {
+            int x = ((a & 1) ^ (b & 1) ^ carry) << i;
+            r = (r | x);
+            carry = ((a & 1) & (b & 1)) | ((a & 1) & carry) | ((b & 1) & carry);
+            a = (a >> 1);
+            b = (b >> 1);
+        }
+        return r;
+    }
+};
