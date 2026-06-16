0988bef1e127fe7dec3f5509753b5a8feebb7844
Connor Burch <118960765+Linelander@users.noreply.github.com>
2026-06-15 22:24:04 -0400

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        stones = sorted(stones)
+        print(stones)
+        while len(stones) > 1:
+            biggest = stones.pop(len(stones) - 1)
+            stones[len(stones) - 1] = biggest - stones[len(stones) - 1]
+            stones = sorted(stones)
+        return stones[0]
