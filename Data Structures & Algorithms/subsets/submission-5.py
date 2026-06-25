99fcb9df9724feeeff37f1ef6a1bc4d59583ba74
Connor Burch <118960765+Linelander@users.noreply.github.com>
2026-06-24 23:25:01 -0400

    Add: subsets - submission-5

---
+++ Data Structures & Algorithms/subsets/submission-5.py
@@ -0,0 +1,12 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        def recur(x, sub):
+            result.append(sub[:])
+            for i in range (x, len(nums)):
+                sub.append(nums[i])
+                recur(i + 1, sub[:])
+                sub.pop()
+
+        recur(0, [])
+        return result
