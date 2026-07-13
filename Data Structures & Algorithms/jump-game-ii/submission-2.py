7f4ea5a43984e7941995382ef72462d1445e367d
Connor Burch <118960765+Linelander@users.noreply.github.com>
2026-07-12 21:47:43 -0400

    Add: jump-game-ii - submission-2

---
+++ Data Structures & Algorithms/jump-game-ii/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        memo = {}
+
+        def dfs(i):
+            if i >= len(nums) - 1: # goal is the final space, not to leave the array
+                return 0
+            if i in memo:
+                return memo[i]
+            
+            result = float('inf')
+            for ni in range(i+1, i+nums[i] + 1):
+                result = min(result, 1 + dfs(ni))
+            memo[i] = result
+            return result
+
+        return dfs(0)
+        # TODO: suboptimal
