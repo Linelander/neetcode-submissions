f74976e447858d3b2e8ca93b1dd1f189479adcd8
Connor Burch <118960765+Linelander@users.noreply.github.com>
2026-07-26 20:52:39 -0400

    Add: house-robber - submission-11

---
+++ Data Structures & Algorithms/house-robber/submission-11.py
@@ -0,0 +1,8 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        hauls = nums + [0] * 3
+
+        for i in range(len(nums)-1, -1, -1):
+            hauls[i] = hauls[i] + max(hauls[i+2], hauls[i+3])
+
+        return max(hauls[0], hauls[1])
